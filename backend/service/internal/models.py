1a1d108a579d3a92f1392eb992fa4e30b863a2a1
Joseph Baruch <josephbaruch48@gmail.com>
2025-02-10 15:20:31 -0800

    Updating user model and making it expandable

--- backend/service/internal/models.py
+++ backend/service/internal/models.py
@@ -5,7 +5,10 @@ class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128, blank=False, default="")
     created_at = models.DateTimeField(auto_now_add=True)
+    admin = models.BooleanField(default=False)
+    budgetName = models.CharField(max_length=30, default="")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
