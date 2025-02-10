e842b97105c1b115f9637c072cf488ff88af7e91
Joseph Baruch <josephbaruch48@gmail.com>
2025-02-10 15:51:30 -0800

    Styling improvements

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,44 +3,64 @@ import Transactions from './Components/Transactions';
 import ViewTransactions from './Components/ViewTransactions';
 import ListUsers from './Components/ListUsers';
 import CreateUser from './Components/CreateUser';
-// import { makeStyles } from '@mui/styles';
+import { makeStyles } from '@mui/styles';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
-// const useStyles = makeStyles({
-//   root: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     justifyContent: 'center',
-//     width: '100vw',
-//     textAlign: 'center',
-//   }
-// });
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '100vw',
+    textAlign: 'center',
+  },
+  appBar: {
+    marginBottom: '20px',
+  },
+  link: {
+    color: 'white',
+    textDecoration: 'none',
+    marginRight: '15px',
+  },
+  title: { 
+    flexGrow: 1,
+  }
+});
 
 function App() {
-  // const classes = useStyles();
+  const classes = useStyles();
 
   return (
     <Router>
-    <div>
-      <h1>GrantManagement</h1>
-
-      <Link to="/transactions">Make Transaction</Link>
-      <br />
-      <Link to="/all-transactions">View transactions</Link>
-      <br />
-      <Link to="/users">Create users</Link>
-      <br />
-      <Link to="/list-users">List users</Link>
-      <br />
-      
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/transactions" element={<Transactions />} />
-        <Route path="/all-transactions" element={<ViewTransactions />} />
-        <Route path="/list-users" element={<ListUsers />} />
-        <Route path="/users" element={<CreateUser />} />
-      </Routes>
-    </div>
-  </Router>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            GrantManagement
+          </Typography>
+          <Button color="inherit">
+            <Link to="/transactions" className={classes.link}>Make Transaction</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="/all-transactions" className={classes.link}>View Transactions</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="/users" className={classes.link}>Create Users</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to="/list-users" className={classes.link}>List Users</Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.root}>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/all-transactions" element={<ViewTransactions />} />
+          <Route path="/list-users" element={<ListUsers />} />
+          <Route path="/users" element={<CreateUser />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
