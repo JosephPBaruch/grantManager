9debb53f8d146b102bb14c951e2ff9e127f7c55a
Joseph Baruch <josephbaruch48@gmail.com>
2025-02-10 14:47:51 -0800

    Transaction views

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,36 @@
-import { makeStyles } from '@mui/styles';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Transactions from './Transactions';
+import ViewTransactions from './ViewTransactions';
+// import { makeStyles } from '@mui/styles';
 
-const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    width: '100vw',
-    textAlign: 'center',
-  }
-});
+// const useStyles = makeStyles({
+//   root: {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     justifyContent: 'center',
+//     width: '100vw',
+//     textAlign: 'center',
+//   }
+// });
 
 function App() {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <h1>Grant Budget Manager</h1>
+    <Router>
+    <div>
+      <h1>GrantManagement</h1>
+
+      <Link to="/transactions">Make Transaction</Link>
+      <br />
+      <Link to="/all-transactions">View transactions</Link>
+      <Routes>
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/all-transactions" element={<ViewTransactions />} />
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
     </div>
+  </Router>
   );
 }
 
