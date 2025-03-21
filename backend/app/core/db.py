d8ae39a7f1cc283cbe68b7ecc7d5f01d4cba6c79
Nathan Hampton <nathanhampton40000@gmail.com>
2025-03-21 12:47:36 -0700

    enabled plpy on startup

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -3,6 +3,7 @@ import logging
 from app import crud
 from app.core.config import settings
 from app.models import User, UserCreate
+from sqlalchemy import event
 from sqlmodel import Session, create_engine, select
 
 logging.basicConfig(level=logging.INFO)
@@ -10,6 +11,21 @@ logger = logging.getLogger(__name__)
 
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+
+
+ENABLE_PLPYTHON = "CREATE EXTENSION IF NOT EXISTS plpython3u"
+
+
+def create_triggers(session: Session):
+    pass
+
+
+# Enable PLPython on connection
+@event.listens_for(engine, "connect", insert=True)
+def enable_pl_python(dbapi_connection, connection_record):
+    cursor_obj = dbapi_connection.cursor()
+    cursor_obj.execute(ENABLE_PLPYTHON)
+    cursor_obj.close()
 
 
 def init_db(session: Session) -> None:
@@ -26,4 +42,5 @@ def init_db(session: Session) -> None:
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
         )
+        user = crud.create_user(session=session, user_create=user_in)
         user = crud.create_user(session=session, user_create=user_in)
