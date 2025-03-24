c80e5068518bbe8cfe22e08ab82fbf1393852681
Joseph Baruch <josephbaruch48@gmail.com>
2025-03-23 17:38:12 -0700

    fix to compose and signup

--- client/src/Components/SignUp.tsx
+++ client/src/Components/SignUp.tsx
@@ -22,30 +22,28 @@ const useStyles = makeStyles({
 const SignUp: React.FC = () => {
   const classes = useStyles();
   const navigate = useNavigate();
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    navigate("/sign-in");
-    // fetch('http://127.0.0.1:8080/api/users/', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({ first_name: firstName, last_name: lastName, email, password }),
-    // }).then((response) => {
-    //   if (response.ok) {
-    //     console.log('User signed up!');
-    //     navigate("/sign-in");
-    //   } else {
-    //     console.error('Error signing up:', response.statusText);
-    //   }
-    // }).catch((error) => {
-    //   console.error('Error signing up:', error);
-    // });
+    fetch('http://localhost:8000/api/v1/users/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password, full_name: fullName }),
+    }).then((response) => {
+      if (response.ok) {
+        console.log('User signed up!');
+        navigate("/");
+      } else {
+        console.error('Error signing up:', response.statusText);
+      }
+    }).catch((error) => {
+      console.error('Error signing up:', error);
+    });
   };
 
   return (
@@ -58,18 +56,9 @@ const SignUp: React.FC = () => {
       </Typography>
       <form onSubmit={handleSubmit} className={classes.form}>
         <TextField
-          label="First Name"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          fullWidth
-          margin="normal"
-          required
-          className={classes.textField}
-        />
-        <TextField
-          label="Last Name"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
+          label="Full Name"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
           fullWidth
           margin="normal"
           required
